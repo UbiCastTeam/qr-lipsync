d4463c6d768024867b262691479fb9627b370bb9
Geoffrey Guéret <geoffrey.gueret@ubicast.eu>
2020-02-26 19:42:39 +0100

    Revert "defined GST_PLUGIN_PATH to find qroverlay"
    This reverts commit c0d4cd71e29b62914b8fa184a7f75ba571643c61.

--- setup.py
+++ setup.py
@@ -1,22 +1,2 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Copyright 2017, Florent Thiery
-
 from setuptools import setup
-
-setup(
-    name='qrlipsync',
-    version=1.0,
-    description='Tool for checking A/V synchronization',
-    url='https://github.com/UbiCastTeam/qr-lipsync',
-    scripts=[
-        'bin/qr-lipsync-analyze.py',
-        'bin/qr-lipsync-detect.py',
-        'bin/qr-lipsync-generate.py',
-    ],
-    setup_requires=[
-        'setuptools >= 3.3',
-    ],
-    packages=['qrlipsync'],
-    test_suite="tests"
-)
+setup(setup_requires="setuptools>=30.3.0")
